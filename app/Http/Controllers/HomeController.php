d3ea5e279f12dd059a79b97db2e3174424f83bb5
Josue Plaza <plazajosue2@gmail.com>
2018-08-18 12:29:05 -0500

    api configurations; send email setup

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function sendEmail(Request $request) {
+        // Receive the contact from the frontend
+        // send to an email
+    }
+}
